b3040ef9ace94f2431c0843aaa686f99b3b2aa5e
Swapnil Pathak <61601014+swapnilpathak14@users.noreply.github.com>
2023-01-30 14:28:48 +0530

    New Changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
 public class Main {
+  
+  public static class Product{
+
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Product pd=new Product();
+
+
+        pd.product(1,2);
+        pd.product(1,2,3);
+        pd.product(1.2,1.4);
+
+
+
+    }
 
 }
